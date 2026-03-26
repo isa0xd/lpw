e3d8fbab32cc63bd4e7f71581af3103e1147382e
Isa Araújo <isabelaa.ifpr2024@gmail.com>
2026-03-26 15:09:48 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jogo</title>
+
+</head>
+
+<body>
+
+<h1>Escolha uma opção como palpite</h1>
+
+    <li><a href="jogo.php?palpite=0">Opção 1</a></li>
+    <br>
+    <li><a href="jogo.php?palpite=0">Opção 2</a></li>
+    <br>
+    <li><a href="jogo.php?palpite=0">Opção 3</a></li>
+    <br>
+    <li><a href="jogo.php?palpite=0">Opção 4</a></li>
+
+</body>
+
+</html>
